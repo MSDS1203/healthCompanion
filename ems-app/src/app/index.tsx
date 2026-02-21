d9dd4e5abe2059b7f092ed1bbf81ee4e5e371851
Kailee Grubbs <kailee.grubbs@gmail.com>
2026-02-21 18:54:22 -0500

    Create the home page interface

--- ems-app/src/app/index.tsx
+++ ems-app/src/app/index.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { useRouter } from 'expo-router';
+
 
 export default function HomeScreen() {
+    const router = useRouter();
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to your emergency response companion</Text>
+      <Pressable 
+        style={styles.emergencyBtn}
+        onPress={() => {router.push("/ems")}}
+      >
+        <MaterialCommunityIcons name="alert-decagram" size={35} color="white" />        
+        <Text style={styles.emergencyFont}>EMERGENCY</Text>
+      </Pressable>
+      <Pressable 
+        style={styles.moreInfoBtn}
+        onPress={() => {router.push("/get-info")}}
+      >
+        <Text style={styles.infoFont}>More Information</Text>
+      </Pressable>
     </View>
   );
 }
@@ -16,5 +32,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20,
+    margin: 20,
   },
+  emergencyBtn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+    backgroundColor: '#CF0624',
+    height: '50%',
+    width: '100%',
+    borderRadius: 20,
+  },
+  emergencyFont: {
+    color: '#fff',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  moreInfoBtn: {
+    backgroundColor: '#AAE0FA',
+    borderRadius: 20,
+    padding: 20,
+  },
+  infoFont: {
+    color: '#021552',
+    fontSize: 20,
+  }
 });
