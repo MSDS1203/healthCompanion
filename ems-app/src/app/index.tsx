b9393b275426e02db7adcccdfa9e310747a451d4
dkmath <dkmath@unc.edu>
2026-02-21 14:45:45 -0500

    initial branch work

--- ems-app/src/app/index.tsx
+++ ems-app/src/app/index.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RedButton from '../components/RedButton';
 
 export default function HomeScreen() {
+  const handlePress = () => {
+    alert('Button pressed!');
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RedButton onPress={handlePress} title="Red Button" color="green" />
+      <RedButton onPress={handlePress} />
     </View>
   );
 }
